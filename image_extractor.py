16a033a960ebe52117f6966f68d8e1f3cd166b4a
a-wels <a_wels03@uni-muenster.de>
2020-12-21 10:28:13 +0100

    updates

--- image_extractor.py
+++ image_extractor.py
@@ -11,19 +11,38 @@ for dir in os.walk(path):
     for file in os.listdir(dir[0]):
         if  file.endswith(".tif") or file.endswith(".tiff") or file.endswith(".png") or file.endswith(".PNG"):
             if not os.path.exists(target):
-                os.makedirs(target)    
+                os.makedirs(target)   
             img_path = dir[0]+"/"+file
             print(img_path)
             image = cv2.imread(img_path)
-            x=5100
-            y=7015
+            bordersize = 10
+            x=5110
+            y=7025
 
-            image = image[20:y-50, 5:x-5]
+            image = image[20:y-40, 5:x-5]
+  
 
             gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+            hsv=cv2.cvtColor(image,cv2.COLOR_BGR2HSV)
+
+            # Define lower and uppper limits of what we call "brown"
+            brown_lo=np.array([0,0,0])
+            brown_hi=np.array([13,13,13])
+
+            # Mask image to only select browns
+            mask=cv2.inRange(hsv,brown_lo,brown_hi)
+
+            # Change image to red where we found brown
+            gray[mask>0]=(255)
+
+            
+          #  imS = cv2.resize(image, (1280, 720))                    # Resize image
+        #    cv2.imshow("Image", imS)
+         #   cv2.waitKey(0)
             gray = cv2.GaussianBlur(gray, (5, 5), 0)
 
             ret, th = cv2.threshold(gray,210,235,1)
+
             
             cnts, hierarchy = cv2.findContours(th.copy(),cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
             cnts = sorted(cnts, key = cv2.contourArea, reverse = True)
@@ -33,13 +52,13 @@ for dir in os.walk(path):
                 box = cv2.cv.BoxPoints(box) if imutils.is_cv2() else cv2.boxPoints(box)
                 box = np.array(box, dtype="int")
                 Area = image.shape[0]*image.shape[1]
-                if Area/10 < cv2.contourArea(box) < Area*2/3:
+                if Area/10 < cv2.contourArea(box) < Area*1/4:
                     x, y, width, height = cv2.boundingRect(c)
                     roi = image[y:y+height, x:x+width]
                     print(target+str(i)+"-"+file)
                     try:
                         cv2.imwrite(target+str(i)+"-"+file, roi)
-                        cv2.drawContours(image, [box], -1, (0, 255, 0), 20)
+                      #  cv2.drawContours(image, [box], -1, (0, 255, 0), 20)
                         i = i+1
 
 
